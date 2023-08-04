5ff7159d5b65c2db058ec41c8102df64716334de
Tốt <totlv99@gmail.com>
2023-08-04 17:39:50 +0700

    crud category

--- src/main/java/com/webservice/repository/ICategoryRepository.java
+++ src/main/java/com/webservice/repository/ICategoryRepository.java
@@ -5,6 +5,9 @@ import com.webservice.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ICategoryRepository extends JpaRepository<Category, Integer> {
+    List<Category> findByParent(String parent);
 }
--- src/main/java/com/webservice/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/webservice/service/impl/CategoryServiceImpl.java
@@ -1,12 +1,16 @@
 package com.webservice.service.impl;
 
 import com.webservice.model.Category;
+import com.webservice.model.dto.ParentDTO;
 import com.webservice.repository.ICategoryRepository;
 import com.webservice.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class CategoryServiceImpl implements ICategoryService {
@@ -36,5 +40,23 @@ public class CategoryServiceImpl implements ICategoryService {
     @Override
     public void deleteById(int id) {
         iCategoryRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Category> getByParent(String parent) {
+        return iCategoryRepository.findByParent(parent);
+    }
+
+    @Override
+    public List<ParentDTO> getAllParentDTO() {
+        Set<String> parents = new HashSet<>();
+        for (Category category : getAll()) {
+            parents.add(category.getParent());
+        }
+        List<ParentDTO> parentDTOs = new ArrayList<>();
+        for (String s : parents) {
+            parentDTOs.add(new ParentDTO(getByParent(s)));
+        }
+        return parentDTOs;
     }
 }
